a7d05c404db3fa9f6b901c55bb54c3ad03ab492d
shamanth66 <143588706+shamanth66@users.noreply.github.com>
2025-06-26 20:47:40 +0530

    Delete Student Budget Tracker directory

--- Student Budget Tracker/Student Budget Tracker/static/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	$('.delete-transaction').click(function () {
-		var id = $(this).attr('data-id');
-		var url = $(this).attr('data-url');
-
-		$('.modal-form', 'input').val(id);
-		$('.modal-form').attr('action', url);
-	});
-});
